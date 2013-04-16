29c14978ebfefdb0a16bab7ea651f9bbf0f10a35
zpeng <pengziyang@gmail.com>
2013-04-16 16:43:02 +0100

    Using jquery ajax for updating the menu item for cms

--- modules/cms/admin/control/menu_update.php
+++ modules/cms/admin/control/menu_update.php
@@ -1,7 +1,6 @@
 <?
 require_once('../../../../includes/bootstrap.php');
 
-$module_code = secureRequestParameter($_REQUEST["module_code"]);
 $menu_id = secureRequestParameter($_REQUEST["menu_id"]);
 $menu_name = secureRequestParameter($_REQUEST["menu_name"]);
 $menu_link = secureRequestParameter($_REQUEST["menu_link"]);
@@ -16,10 +15,8 @@ $menu->set_menu_order($menu_order);
 $menu->set_menu_desc($menu_desc);
 $menu->update();
 
-$url = SERVER_URL . "admin/main.php?module_code=" . $module_code . "&view=menu_list"; // target of the redirect
-$msg = "The menu item [" . $menu_name . "] has been updated";
-$url = $url . "&info=" . $msg;
-
-header("Location: " . $url);
+$response_array['status'] = 'success';
+header('Content-type: application/json');
+echo json_encode($response_array);
 
 ?>
--- modules/cms/admin/view/menu_update.php
+++ modules/cms/admin/view/menu_update.php
@@ -1,31 +1,30 @@
 <h1 class="content_title">Update Menu</h1>
-<? include_once('view/notification_bar.php') ?>
+<div id="notification"></div>
 <div id="content">
     <?
     $menu_id = secureRequestParameter($_REQUEST["menu_id"]);
-    $module_code = secureRequestParameter($_REQUEST["module_code"]);
-
     $menu = new Menu();
     $menu->load($menu_id);
     ?>
     <br/>
-    <form id="MenuUpdateForm" action="<?= SERVER_URL ?>modules/cms/admin/control/menu_update.php" method="post">
-        <input type="hidden" value="<? echo $menu_id ?>" name="menu_id"/>
-        <input type="hidden" value="<? echo $module_code ?>" name="module_code"/>
+
+    <form id="MenuUpdateForm"  method="post">
+        <input type="hidden" value="<? echo $menu_id ?>" name="menu_id" id="menu_id"/>
         <table class="inputTable">
             <tr>
                 <td width="150" align="right"><b>Menu Name: </b></td>
-                <td><input name="menu_name" id="menu_name" value="<?=$menu->get_menu_name()?>" style="width: 400px;"/>
+                <td><input name="menu_name" id="menu_name" value="<?= $menu->get_menu_name() ?>" style="width: 400px;"/>
                 </td>
             </tr>
             <tr>
                 <td width="150" align="right"><b>Menu Link: </b></td>
-                <td><input name="menu_link" id="menu_link" value="<?=$menu->get_menu_link()?>" style="width: 400px;"/>
+                <td><input name="menu_link" id="menu_link" value="<?= $menu->get_menu_link() ?>" style="width: 400px;"/>
                 </td>
             </tr>
             <tr>
                 <td width="150" align="right"><b>Menu Order: </b></td>
-                <td><input name="menu_order" id="menu_order" value="<?=$menu->get_menu_order()?>" style="width: 100px;"/>
+                <td><input name="menu_order" id="menu_order" value="<?= $menu->get_menu_order() ?>"
+                           style="width: 100px;"/>
                 </td>
             </tr>
             <tr>
@@ -42,7 +41,7 @@
     <script>
         jQuery("#update_btn").button();
 
-        jQuery(function(){
+        jQuery(function () {
             jQuery("#menu_name").validate({
                 expression: "if (VAL) return true; else return false;",
                 message: "Please enter a valid menu name"
@@ -55,6 +54,38 @@
                 expression: "if (VAL.match(/^[0-9]*$/) && VAL) return true; else return false;",
                 message: "Please enter a valid menu name"
             });
+
+            jQuery('form#MenuUpdateForm').validated(function () {
+                var menu_id = $("#menu_id").val();
+                var menu_name = $("#menu_name").val();
+                var menu_link = $("#menu_link").val();
+                var menu_order = $("#menu_order").val();
+                var menu_desc = $("#menu_desc").val();
+
+                $.ajax({
+                    url: SERVER_URL + "modules/cms/admin/control/menu_update.php",
+                    type: "POST",
+                    data: {menu_id: menu_id,
+                        menu_name: menu_name,
+                        menu_link: menu_link,
+                        menu_order: menu_order,
+                        menu_desc: menu_desc
+                    },
+                    dataType: "json",
+                    success: function (data) {
+                        if (data.status == "success") {
+                            jQuery("div#notification").html("<span class='info'>Menu item has been updated successfully!</span>");
+                        } else {
+                            jQuery("div#notification").html("<span class='error'>Unable to update this menu item. Try again please!</span>");
+                        }
+                    },
+                    error: function () {
+                        jQuery("div#notification").html("<span class='warning'>There was a connection error. Try again please!</span>");
+                    }
+                });
+                return false;
+            });
+
         });
     </script>
 </div>
