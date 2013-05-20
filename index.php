acc4d8f43d8fa0e17905a8d9259c142735df99a9
zpeng <pengziyang@gmail.com>
2013-05-20 12:01:30 +0100

    fixing the footer alignment issue

--- index.php
+++ index.php
@@ -6,20 +6,23 @@ use modules\core\includes\classes\ConfigurationManager;
 $configManager = new ConfigurationManager();
 echo  outputHTMLStartFrontend($JS_GLOBAL, array("css/style.css"), $configManager) ?>
 
+<div id="wapper">
 <? include_once('view/top_header.php') ?>
 
+<div id="main_body">
+    <div id="main_content">
+        <div id="left_content">
+            <? include_once('view/deal_of_the_day.php') ?>
 
-    <div id="main_body">
-        <div id="main_content">
-            <div id="left_content">
-            </div>
+            <? include_once('view/deal_list.php') ?>
+        </div>
 
-            <div id="right_content">
-
-            </div>
+        <div id="right_content">
+            <? include_once('view/supplier_list.php') ?>
         </div>
     </div>
-
-
+</div>
 <? include_once('view/footer.php') ?>
+</div>
+
 <?= outputHTMLEnd() ?>
