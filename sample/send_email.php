ce8ad9f7b2c632a5168c97655343522a9f121cc6
zpeng <pengziyang@gmail.com>
2013-08-25 20:49:52 +0100

    update send email example

--- sample/send_email.php
+++ sample/send_email.php
@@ -1,15 +1,36 @@
 <?php
 
+require_once('../includes/bootstrap.php');
 require_once('../external/php/Eden-3.1/eden.php');
+
+
+
+use modules\deal_steal\includes\classes\EmailTemplateManager;
+use modules\deal_steal\includes\classes\Template;
+
+$data["client_name"] = "Asif Awan";
+$data["new_password"] = "ABCDEFGEEDF111";
+
+$emailTemplateManager = new EmailTemplateManager();
+$template = new Template();
+$content = "";
+if ($template->loadByKey("customer_password_reset")) {
+    try {
+        $content =  $emailTemplateManager->getProcessedContentFromTemplate($template->getContent(),$data);
+    } catch (Exception $e) {
+        throw new Exception("Unable to process the template due to the following error \n\n\n" . $e->getMessage() . "\n");
+    }
+} else {
+    throw new Exception("Template " . $template_key . " is not found!");
+}
 
 eden()->setLoader(true);
 
 $smtp = eden('mail')->smtp('auth.smtp.1and1.co.uk', 'admin@dealsteal.co', '19840617', 25);
 
 $smtp->setSubject('Welcome!')
-    ->setBody('<p>Hello you!</p>', true)
-    ->setBody('Hello you!')
-    ->addTo('pengziyang@gmail.com')
+    ->setBody($content , true)
+    ->addTo('aawan311@gmail.com')
     ->send();
 
 $smtp->disconnect();
